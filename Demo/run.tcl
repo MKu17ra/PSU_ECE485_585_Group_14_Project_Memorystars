97738d96d26aebc3ba76c4fd86b2769aeb9b9093
wekster <31289154+31415pi@users.noreply.github.com>
2021-10-29 20:13:47 -0700

    Create run.tcl

---
+++ Demo/run.tcl
@@ -0,0 +1,9 @@
+vlib -quiet ece585-dram
+
+vlog -quiet -writetoplevels questa.tops design.sv
+
+# Debug
+#vsim -f questa.tops -batch -do "vsim -voptargs=+acc=npr; run -all; exit" -voptargs=+acc=npr
+# No Debug
+#vsim -quiet -f questa.tops -batch +infile=input_run.txt -do "vsim ; run -all; exit"
+vsim -quiet -f questa.tops -batch -do "vsim ; run -all; exit"
